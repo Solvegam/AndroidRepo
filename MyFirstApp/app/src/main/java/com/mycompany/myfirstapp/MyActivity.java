1a50fb775d9ee4ffa1890c80c57f37c54c706787
Solvegam <solvegam2011@yandex.ua>
2015-10-19 22:20:46 +0300

    Some features were added

--- MyFirstApp/app/src/main/java/com/mycompany/myfirstapp/MyActivity.java
+++ MyFirstApp/app/src/main/java/com/mycompany/myfirstapp/MyActivity.java
@@ -1,5 +1,8 @@
 package com.mycompany.myfirstapp;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,8 +11,11 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 
 public class MyActivity extends AppCompatActivity {
+
+    public final static String EXTRA_MESSAGE = "com.mycompany.myfirstapp.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,10 +28,45 @@ public class MyActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "You thought it would be easy?", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Did you think it would be easy?", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
+        notify("onCreate");
+    }
+
+    /** Обрабатывает нажатие на кнопку Send*/
+    public void sendMessage (View view)
+    {
+        Intent intent = new Intent (this,MessageDisplayingActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String text = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE,text);
+        startActivity(intent);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        notify("onPause");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        notify("onResume");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        notify("onStart");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        notify("onStop");
     }
 
     @Override
@@ -48,5 +89,17 @@ public class MyActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void notify(String methodName) {
+        String name = this.getClass().getName();
+        String[] splittedName = name.split("\\.");
+        Notification notification = new Notification.Builder(this)
+                .setContentTitle(methodName + " " + splittedName[splittedName.length - 1]).setAutoCancel(true)
+                .setSmallIcon(R.drawable.ic_launcher)
+                .setContentText(name).build();
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        notificationManager.notify((int) System.currentTimeMillis(), notification);
     }
 }
